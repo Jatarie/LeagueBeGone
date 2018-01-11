34784216b6cd65cca12d152801e5af8b752b1899
Matthew Smyth <matthew.e.j.smyth@gmail.com>
2018-01-11 15:27:40 +0000

    added sum of frame

--- Source/VodDownloader.py
+++ Source/VodDownloader.py
@@ -6,60 +6,10 @@ import sys
 import math
 from cv2 import VideoCapture, imwrite, imread
 import grequests
+import numpy
 
 
-sys.path.append('../')
-from Source import keyfile
-
-
-def twitchAPIRequest(vodID):
-    rawTwitchAPIJsonData = requests.get("https://api.twitch.tv/api/vods/"
-                                        "{}/access_token?&client_id={}".format(vodID, keyfile.client_id))
-    jsonData = rawTwitchAPIJsonData.json()
-    token = jsonData["token"]
-    sig = jsonData["sig"]
-    return token, sig
-
-
-def usherAPIRequest(token, sig, vodID):
-    rawUsherAPIData = requests.get("http://usher.twitch.tv/vod/"
-                                   "{}?nauthsig={}&nauth={}&allow_source=true".format(vodID, sig, token))
-    m3u8links = re.findall(r'(http.+?m3u8)', rawUsherAPIData.text)
-    source_m3u8link = m3u8links[0]
-    low_m3u8link = m3u8links[5]
-    raw_m3u8link_data = requests.get(source_m3u8link)
-    segment_length = int(re.findall(r'(?<=EXTINF:)[0-9]', raw_m3u8link_data.text)[0])
-    extension_list = (re.findall(r'\n([^#]+)\n', raw_m3u8link_data.text))
-
-    source_m3u8linkm_link = re.findall(r'(.+?)(?=index)', source_m3u8link)
-    low_m3u8link_link = re.findall(r'(.+?)(?=index)', low_m3u8link)
-    return extension_list, segment_length, source_m3u8linkm_link, low_m3u8link_link
-
-
-def fileHandler(vodID, dir_path):
-    voddir = os.path.pardir + "\\vods\\"
-    try:
-        os.mkdir(voddir)
-    except FileExistsError:
-        pass
-    # try:
-    #     os.mkdir(os.path.pardir + "\\images\\")
-    # except FileExistsError:
-    #     pass
-    file_list = os.listdir(voddir)
-    for file in file_list:
-        os.remove(voddir + file)
-    # file_list = os.listdir(dir_path)
-    # for file in file_list:
-    #     if ".jpg" in file:
-    #         os.remove(file)
-
-    filepath = voddir + str(vodID) + ".mp4"
-    try:
-        os.remove(filepath)
-    except FileNotFoundError:
-        pass
-    return filepath
+client_id = "map2eprcvghxg8cdzdy2207giqnn64"
 
 
 def progressbar(numerator, denominator, time_remaining):
@@ -91,6 +41,58 @@ def timeremaining(start, extension_list, i):
     chunks_remaining = len(extension_list) - i
     ave_chunk_completion_time = time_elapsed / chunks_completed
     return ave_chunk_completion_time * chunks_remaining
+
+
+def getChannelVodID(Channel):
+    r = requests.get("https://api.twitch.tv/kraken/channels/"
+                     "{}/videos?client_id=map2eprcvghxg8cdzdy2207giqnn64&broadcast_type=archive".format(Channel))
+    data = r.json()
+    return int(data["videos"][0]["url"][29:])
+
+
+def getFirstFrameData(file, frame_counter):
+    cap = VideoCapture(file)
+    ret, frame = cap.read()
+    imwrite("frame{}.jpg".format(frame_counter), frame)
+    img = imread("frame{}.jpg".format(frame_counter))
+    # os.remove("frame.jpg")
+    if img is None:
+        return None
+    else:
+        return img
+
+
+def analyseFirstFrameOfVideoChunk(r, frame_number):
+    with open("chunk.mp4", "wb") as f:
+        saveChunk(f, r)
+    cap = VideoCapture("chunk.mp4")
+    ret, frame = cap.read()
+    imwrite("frame{}.jpg".format(frame_number), frame)
+    img = imread("frame{}.jpg".format( frame_number))
+    os.remove("frame{}.jpg".format(frame_number))
+    if img is None:
+        return frame_number, False
+    px1 = img[1056, 1893]
+    px2 = img[1040, 1000]
+    px3 = img[801, 1830]
+    true_list = []
+    if px1[0] < 10 and px1[1] < 10 and px1[2] < 10:
+        true_list.append(True)
+    if 50 > px2[0] > 15 and 40 > px2[1] > 20 and 40 > px2[2] > 10:
+        true_list.append(True)
+    if 70 > px3[0] > 40 and 110 > px3[1] > 80 and 130 > px3[2] > 90:
+        true_list.append(True)
+    if len(true_list) >= 2:
+        # os.rename("frame{}.jpg".format(frame_number), "{}-LEAGUE-{}-{}-{}.jpg".format(frame_number, px1, px2, px3))
+        return frame_number, True
+    # os.rename("frame{}.jpg".format(frame_number), "{}-Normal-{}-{}-{}.jpg".format(frame_number, px1, px2, px3))
+    return frame_number, False
+
+
+def saveChunk(f, r):
+    for chunk in r.iter_content(chunk_size=255):
+        if chunk:
+            f.write(chunk)
 
 
 def downloadChunks(extension_list, source_link, filepath, filter_league, segment_length):
@@ -131,52 +133,6 @@ def downloadChunks(extension_list, source_link, filepath, filter_league, segment
           .format(mbDownloaded, elapsedTime, (mbDownloaded / elapsedTime)))
 
 
-def getChannelVodID(Channel):
-    r = requests.get("https://api.twitch.tv/kraken/channels/"
-                     "{}/videos?client_id=map2eprcvghxg8cdzdy2207giqnn64&broadcast_type=archive".format(Channel))
-    data = r.json()
-    return int(data["videos"][0]["url"][29:])
-
-
-def getFirstFrameData(file):
-    cap = VideoCapture(file)
-    ret, frame = cap.read()
-    imwrite("frame.jpg", frame)
-    img = imread("frame.jpg")
-    os.remove("frame.jpg")
-    if img is None:
-        return None
-    else:
-        return img
-
-
-def analyseFirstFrameOfVideoChunk(r, frame_number):
-    with open("chunk.mp4", "wb") as f:
-        saveChunk(f, r)
-    cap = VideoCapture("chunk.mp4")
-    ret, frame = cap.read()
-    imwrite("frame{}.jpg".format(frame_number), frame)
-    img = imread("frame{}.jpg".format( frame_number))
-    os.remove("frame{}.jpg".format(frame_number))
-    if img is None:
-        return frame_number, False
-    px1 = img[1056, 1893]
-    px2 = img[1040, 1000]
-    px3 = img[801, 1830]
-    true_list = []
-    if px1[0] < 10 and px1[1] < 10 and px1[2] < 10:
-        true_list.append(True)
-    if 50 > px2[0] > 15 and 40 > px2[1] > 20 and 40 > px2[2] > 10:
-        true_list.append(True)
-    if 70 > px3[0] > 40 and 110 > px3[1] > 80 and 130 > px3[2] > 90:
-        true_list.append(True)
-    if len(true_list) >= 2:
-        # os.rename("frame{}.jpg".format(frame_number), "{}-LEAGUE-{}-{}-{}.jpg".format(frame_number, px1, px2, px3))
-        return frame_number, True
-    # os.rename("frame{}.jpg".format(frame_number), "{}-Normal-{}-{}-{}.jpg".format(frame_number, px1, px2, px3))
-    return frame_number, False
-
-
 def trimExtensionList(time_start, time_end, segment_length, extension_list):
     starting_index = int(int(time_start) / int(segment_length))
     if time_end == 0:
@@ -184,6 +140,77 @@ def trimExtensionList(time_start, time_end, segment_length, extension_list):
     else:
         ending_index = int(int(time_end) / int(segment_length))
     return extension_list[starting_index:ending_index]
+
+
+def analyseVod(segment_length, extension_list, low_link):
+    sample_every_n_seconds = 300
+    extensions_to_skip = int(sample_every_n_seconds / segment_length)
+    sample_extention_list = extension_list[::extensions_to_skip]
+    urls = [low_link[0] + sample_extention_list[i] for i in range(len(sample_extention_list))]
+    rs = (grequests.get(u) for u in urls)
+    responses = grequests.map(rs)
+    frame_counter = 0
+    for response in responses:
+        frame_value = 0
+        with open("analysischunk.mp4", "wb") as f:
+            saveChunk(f, response)
+        img = getFirstFrameData("analysischunk.mp4", frame_counter)
+        for row in img:
+            for pixel in row:
+                frame_value += numpy.sum(pixel)
+        print(frame_counter, frame_value)
+        os.rename("frame{}.jpg".format(frame_counter), "frame{} - {}.jpg".format(frame_counter, frame_value))
+        frame_counter += 1
+
+
+def usherAPIRequest(token, sig, vodID):
+    rawUsherAPIData = requests.get("http://usher.twitch.tv/vod/"
+                                   "{}?nauthsig={}&nauth={}&allow_source=true".format(vodID, sig, token))
+    m3u8links = re.findall(r'(http.+?m3u8)', rawUsherAPIData.text)
+    source_m3u8link = m3u8links[0]
+    low_m3u8link = m3u8links[5]
+    raw_m3u8link_data = requests.get(source_m3u8link)
+    segment_length = int(re.findall(r'(?<=EXTINF:)[0-9]', raw_m3u8link_data.text)[0])
+    extension_list = (re.findall(r'\n([^#]+)\n', raw_m3u8link_data.text))
+
+    source_m3u8linkm_link = re.findall(r'(.+?)(?=index)', source_m3u8link)
+    low_m3u8link_link = re.findall(r'(.+?)(?=index)', low_m3u8link)
+    return extension_list, segment_length, source_m3u8linkm_link, low_m3u8link_link
+
+
+def twitchAPIRequest(vodID):
+    rawTwitchAPIJsonData = requests.get("https://api.twitch.tv/api/vods/"
+                                        "{}/access_token?&client_id={}".format(vodID, client_id))
+    jsonData = rawTwitchAPIJsonData.json()
+    token = jsonData["token"]
+    sig = jsonData["sig"]
+    return token, sig
+
+
+def fileHandler(vodID, dir_path):
+    voddir = os.path.pardir + "\\vods\\"
+    try:
+        os.mkdir(voddir)
+    except FileExistsError:
+        pass
+    # try:
+    #     os.mkdir(os.path.pardir + "\\images\\")
+    # except FileExistsError:
+    #     pass
+    file_list = os.listdir(voddir)
+    for file in file_list:
+        os.remove(voddir + file)
+    # file_list = os.listdir(dir_path)
+    # for file in file_list:
+    #     if ".jpg" in file:
+    #         os.remove(file)
+
+    filepath = voddir + str(vodID) + ".mp4"
+    try:
+        os.remove(filepath)
+    except FileNotFoundError:
+        pass
+    return filepath
 
 
 def timeParser(time_start, time_end):
@@ -194,12 +221,6 @@ def timeParser(time_start, time_end):
     time_end = int(h_end)*3600 + int(m_end)*60 + int(s_end)
 
     return time_start, time_end
-
-
-def saveChunk(f, r):
-    for chunk in r.iter_content(chunk_size=255):
-        if chunk:
-            f.write(chunk)
 
 
 def getVideoParams():
@@ -228,19 +249,6 @@ def getVideoParams():
     return VodID, channel, filter_league, time_start, time_end
 
 
-def analyseVod(segment_length, extension_list, low_link):
-    sample_every_n_seconds = 300
-    extensions_to_skip = int(sample_every_n_seconds / segment_length)
-    sample_extention_list = extension_list[::extensions_to_skip]
-    urls = [low_link[0] + sample_extention_list[i] for i in range(len(sample_extention_list))]
-    rs = (grequests.get(u) for u in urls)
-    responses = grequests.map(rs)
-    for response in responses:
-        with open("analysischunk.mp4", "wb") as f:
-            saveChunk(f, response)
-        img = getFirstFrameData("analysischunk.mp4")
-
-
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     vodID, Channel, filter_league, time_start, time_end = getVideoParams()
@@ -248,7 +256,7 @@ def main():
     filepath = fileHandler(vodID, dir_path)
     token, sig = twitchAPIRequest(vodID)
     extension_list, segment_length, source_link, low_link = usherAPIRequest(token, sig, vodID)
-    # analyseVod(segment_length, extension_list, low_link)
+    analyseVod(segment_length, extension_list, low_link)
     extension_list = trimExtensionList(time_start, time_end, segment_length, extension_list)
     downloadChunks(extension_list, source_link, filepath, filter_league, segment_length)
     if os.path.exists("chunk.mp4"):
